0fb3154bddfb6a51efbd4e4e51125f15bb53e7b3
Miguel Blanco <miguel.mendoza@taxfyle.com>
2022-07-28 16:30:11 -0600

    feat(unpkgPathPlugin): resolve nested paths

--- src/plugins/unpkgPathPlugin.tsx
+++ src/plugins/unpkgPathPlugin.tsx
@@ -18,7 +18,8 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes('./') || args.path.includes('../')) {
           return {
             namespace: 'a',
-            path: new URL(args.path, `${args.importer}/`).href,
+            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
+              .href,
           }
         }
 
@@ -37,15 +38,16 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'medium-test-pkg';
+              const message = require('nested-test-pkg');
               console.log(message);
             `,
           }
         }
-        const { data } = await axios.get(args.path)
+        const { data, request } = await axios.get(args.path)
         return {
           loader: 'jsx',
           contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname,
         }
       })
     },
