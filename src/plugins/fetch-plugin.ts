a441c4715bd065dec3b82aa9adad28053a46df85
Miguel Blanco <miguel.mendoza@taxfyle.com>
2022-08-25 12:33:53 -0600

    refactor(fetch-plugin): extract common caching logic

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -6,37 +6,62 @@ import localForage from 'localforage'
 const fileCache = localForage.createInstance({
   name: 'file_cache',
 })
+
 export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
 
     setup(build: esbuild.PluginBuild) {
       // attempt to load (fetch) the 'index.js' file from the unpkg path
-      build.onLoad({ filter: /.*/ }, async (args: OnLoadArgs) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          }
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
         }
+      })
 
-        // Check to see if we have already fetched this file
-        // and if it is in the fileCache
+      build.onLoad({ filter: /.*/ }, async (args: OnLoadArgs) => {
+        // if the package is already cached return
         const cachedResult = await fileCache.getItem<OnLoadResult>(args.path)
 
         if (cachedResult) {
           return cachedResult
         }
+      })
 
+      // attempt to load (fetch) any css package file from the unpkg path
+      build.onLoad({ filter: /.css$/ }, async (args: OnLoadArgs) => {
         const { data, request } = await axios.get(args.path)
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'")
 
+        const contents = `
+            const style = document.createElement('style')
+            style.innerText = '${escaped}'
+            document.head.appendChild(style)
+            `
+
+        const result: OnLoadResult = {
+          loader: 'jsx',
+          contents: contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        }
+
+        // store response in fileCache
+        fileCache.setItem(args.path, result)
+        return result
+      })
+
+      build.onLoad({ filter: /.*/ }, async (args: OnLoadArgs) => {
+        const { data, request } = await axios.get(args.path)
         const result: OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         }
 
-        // store response in fileCache
         fileCache.setItem(args.path, result)
         return result
       })
